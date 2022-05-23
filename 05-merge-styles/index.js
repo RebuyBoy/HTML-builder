190aec6aef66d7dbebfcdad3f75b9965d3d6ad92
Sergey M <marchuk.se@gmail.com>
2022-05-23 16:29:44 +0700

    feat: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,23 @@
+const { readdir, open, readFile } = require('fs/promises');
+const path = require('path');
+
+async function bundleCss(dirPath) {
+  const files = await readdir(dirPath, { withFileTypes: true });
+  const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+  const streamFile = await open(bundlePath, 'w');
+  for (let file of files) {
+    if (file.isFile() && path.extname(file.name) === '.css') {
+      const data = await readFile(path.join(dirPath, file.name), { encoding: 'utf8' });
+      streamFile.appendFile(data);
+    }
+  }
+  streamFile.close(bundlePath);
+}
+
+const cssDirPath = path.join(__dirname, 'styles');
+
+try {
+  bundleCss(cssDirPath);
+} catch (err) {
+  console.log(err);
+}
